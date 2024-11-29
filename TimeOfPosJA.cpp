0d2ecc9aeefacedaf757f3a6bc422fc6f31a7899
joel <joelmichaelabbott@gmail.com>
2024-11-29 13:52:06 -0600

    Commit 2, (Added TimeOfPosJA)

---
+++ TimeOfPosJA.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+using namespace std;
+
+class footballGame {
+private:
+	int quarters;
+	double clock;
+
+public:
+	footballGame() { quarters = 4, clock = 15:00 };
+
+};
